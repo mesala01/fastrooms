5a02cf9244ef82026b0a15b3a7923cc66578a536
Ryan Bennett <bennry01@luther.edu>
2014-05-13 17:21:24 -0500

    typo

--- main.py
+++ main.py
@@ -376,7 +376,7 @@ def error404(e):
 
 @app.errorhandler(500)
 def error500(e):
-	desc="Something major has gone wrong. You should check your log files."
+	desc="Something major has gone wrong. You should check your log files. 500 INTERNAL SERVER ERROR"
 	head="500 - Database error"
 	return render_template('404.html',appname=appname,build=build,desc=desc,head=head), 500
 	
