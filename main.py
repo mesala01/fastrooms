3071c6993ed2a7ced9d722a2d542861d0a6e62c1
Ryan Bennett <bennry01@luther.edu>
2014-05-18 21:54:39 -0500

    maybe now?

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import random
 app = Flask(__name__)
 app.secret_key = os.urandom(30)
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vupejbyhinzrfr:ac3aN9RgjxUKGBvYkemIM7OUTl@ec2-54-83-14-68.compute-1.amazonaws.com:5432/dd4a01s00h2dqr'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(“DATABASE_URL”,”postgresql://pguser:password/dbname”)
 db = SQLAlchemy(app)
 
 appname = config.appname
